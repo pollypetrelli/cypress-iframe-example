ba5e2801f12464f615f44ea8b47b6f0abd94362b
pollypetrelli <pollypetrelli@gmail.com>
2020-06-17 14:17:33 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,10 @@
+//https://www.youtube.com/watch?v=khO8_YAej-s
 Cypress.Commands.add('switchToIframe', (iframe) => {
   return cy
     .get(iframe)
+  // i take 0, contentDocument and body for dev tools Elements as I want to access the body of the iframe
     .its('0.contentDocument.body')
     .should('not.be.empty')
+  //cy.wrap as i want to wrap the custom command and use it with cypress commands like cy.type etc
     .then(cy.wrap);
 });
