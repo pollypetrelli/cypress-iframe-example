2140e6c13195b0d407a167d78d5240139fffde31
pollypetrelli <pollypetrelli@gmail.com>
2020-06-17 14:55:11 +0100

    Update cross-iframe.spec.js

--- cypress/integration/cross-iframe.spec.js
+++ cypress/integration/cross-iframe.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-
+//in order to be able to test iframes with another domain i have to disable chromeWebSecurity
 describe('Cross domain iframes', () => {
   // this test will fail unless you set chromeWebSecurity to false
   it('should let you switch to the iframe', () => {
